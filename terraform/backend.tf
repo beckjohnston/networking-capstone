c4bb1fff62638b86473759f5216a214fb5614c8c
Beck <thebeckster603@gmail.com>
2026-07-29 13:38:49 -0400

    change default region from us-east-1 to us-west-1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "REPLACE_WITH_YOUR_STATE_BUCKET"
     key            = "networking-capstone/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-1"
     dynamodb_table = "networking-capstone-tf-locks"
     encrypt        = true
   }
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 variable "aws_region" {
   type    = string
-  default = "us-east-1"
+  default = "us-west-1"
 }
 
 variable "state_bucket_name" {
