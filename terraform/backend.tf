d1e1e046a4ede93da818accc612846331d56106f
Beck <thebeckster603@gmail.com>
2026-07-29 13:14:28 -0400

    create backend terraform file

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "REPLACE_WITH_YOUR_STATE_BUCKET"
+    key            = "networking-capstone/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "networking-capstone-tf-locks"
+    encrypt        = true
+  }
+}
